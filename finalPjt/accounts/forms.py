25132a7b5c336393d0e8ee2d3a7ea421b6e3cf0b
zoo1 <marulover7@gmail.com>
2019-05-13 05:51:59 +0000

    model 생성

---
+++ finalPjt/accounts/forms.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+
+from django.contrib.auth.models import User
+
+from django.contrib.auth import get_user_model
+from django import forms
+from .models import Profile
+
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username','email','first_name', 'last_name']    
+
+    
+class CustomUserCreationForm(UserCreationForm):
+    
+    class Meta(UserCreationForm.Meta):
+        model = get_user_model()
+        fields = UserCreationForm.Meta.fields
+
+
+class ProfileForm(forms.ModelForm):        
+    class Meta:
+        model = Profile
+        fields =['description', 'nickname', 'image'] 
+        
+
