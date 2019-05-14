11200d2741f565e78da22346cd4db48bb1b8731b
zoo1 <marulover7@gmail.com>
2019-05-14 08:47:07 +0000

    mypage

--- finalPjt/movies/views.py
+++ finalPjt/movies/views.py
@@ -103,14 +103,13 @@ def create_rating(request, movie_id):
         rating.movie = movie
         rating.save()
     # 다음에 다시 상세페이지로 보내기
-    return redirect('movies:list')
+    return redirect('movies:detail', movie.id)
     
-@require_POST
 def delete_rating(request, movie_id, rating_id):
     rating = Rating.objects.get(pk=rating_id)
     if rating.user == request.user:
-        comment.delete()
-    return redirect('movies:list')
+        rating.delete()
+    return redirect('movies:detail', movie_id)
 
 @login_required
 def update_rating(request, movie_id, rating_id):
@@ -120,10 +119,11 @@ def update_rating(request, movie_id, rating_id):
             form = RatingForm(request.POST, instance=rating)
             if form.is_valid():
                 form.save()
-                return redirect('movies:list')
+                return redirect('movies:detail', movie_id)
         else:
             form = RatingForm(instance=rating)
         return render(request, 'movies/update.html', {'form':form})
         
     else:
         return redirect('movies:list')
+
