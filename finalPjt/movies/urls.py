9be282a46404631cc1e1b9c8eb20c788cded427a
zoo1 <marulover7@gmail.com>
2019-05-15 08:45:08 +0000

    git siro...

--- finalPjt/movies/urls.py
+++ finalPjt/movies/urls.py
@@ -17,6 +17,5 @@ urlpatterns = [
     path('<int:movie_id>/rating/create/', views.create_rating, name='create_rating'),
     path('<int:movie_id>/rating/<int:rating_id>/delete', views.delete_rating, name="delete_rating"),
     path('<int:movie_id>/rating/<int:rating_id>/update', views.update_rating, name="update_rating"),
-    path('show/', views.show, name="show"),
 
 ]
