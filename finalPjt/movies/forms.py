171198cba5593288a30680e7fccd213080ed7150
hzzle <hwp0223@gmail.com>
2019-05-16 00:29:36 +0000

    more

--- finalPjt/movies/forms.py
+++ finalPjt/movies/forms.py
@@ -2,6 +2,19 @@ from django import forms
 from .models import Movie, Rating
 
 class MovieForm(forms.ModelForm):
+    title = forms.CharField(label="영화 제목", widget=forms.TextInput(attrs={'placeholder': 'Title'}))
+    director = forms.CharField(label="감독", widget=forms.TextInput(attrs={'placeholder': 'Director'}))
+    nation = forms.CharField(label="국가", widget=forms.TextInput(attrs={'placeholder': 'Nationality'}))
+    openDate = forms.CharField(label="개봉일", widget=forms.TextInput(attrs={'placeholder': 'Released Date'}))
+    watchGrade = forms.CharField(label="관람 등급", widget=forms.TextInput(attrs={'placeholder': 'Grade'}))
+    duration = forms.CharField(label="상영 시간", widget=forms.TextInput(attrs={'placeholder': 'Duration'}))
+    naver_link = forms.CharField(label="네이버 링크", widget=forms.TextInput(attrs={'placeholder': 'Naver Link'}))
+    poster_image = forms.CharField(label="포스터", widget=forms.TextInput(attrs={'placeholder': 'Poster Link'}))
+    summary = forms.CharField(label="줄거리", widget=forms.Textarea(attrs={'placeholder': 'Summary'}))
+    audience = forms.CharField(label="누적관객수", widget=forms.TextInput(attrs={'placeholder': 'Audience Number'}))
+    reservation = forms.CharField(label="예약", widget=forms.TextInput(attrs={'placeholder': 'Reservation Link'}))
+    user_Rating = forms.CharField(label="네이버 평점", widget=forms.TextInput(attrs={'placeholder': 'Naver Rating'}))
+    
     class Meta:
         model = Movie
         fields = ['title', 'genre', 'director', 'nation','openDate','watchGrade',
